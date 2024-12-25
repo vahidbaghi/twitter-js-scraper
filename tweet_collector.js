a66b105f75fa65585b6e326de751c8c2b5a6bb12
Zen <191625328+Zen-CloudLabs@users.noreply.github.com>
2024-12-26 02:02:43 +0330

    Enhance functionality and add tweet links

--- tweet_collector.js
+++ tweet_collector.js
@@ -1,39 +1,74 @@
-let i = 1;
-let dict = {};
-let like_regex = /\b(\d+) Like|Likes\b/;
-let retweet_regex = /\b(\d+) Retweet|Retweets\b/;
-let reply_regex = /\b(\d+) Repl(ies|y)\. Reply\b/;
-let time_regex =  /\btime datetime=\"(.*?)\">\b/;
-let status_regex = /\b\/status\/(\d+)\b/;
-            
-function loop() {      
-    setTimeout(function() {
-        window.scrollBy(0,1000);
-	let element = document.getElementsByClassName("css-1dbjc4n r-1iusvr4 r-16y2uox r-1777fci r-kzbkwu");
-        for(let index=0; index<element.length; index++) {
-            try {
-	        let reply = element[index].innerHTML.match(reply_regex);
-	        let like = element[index].innerHTML.match(like_regex);
-	        let retweet = element[index].innerHTML.match(retweet_regex);
-	        let time = element[index].innerHTML.match(time_regex);
-	        let status = element[index].innerHTML.match(status_regex);
-	        dict[element[index].innerText.split("\n")[1].concat("-"+time[1])] = {
-		    "like":`${like[1]}`,
-		    "reply":`${reply[1]}`,
-		    "retweet":`${retweet[1]}`,
-		    "time":`${time[1]}`,
-		    "text":`${element[index].innerText}`,
-		    "status":`${status[1]}`
-	        };
-           } 
-           catch (error) {
-	       console.log(error)
-           }
-        }
-        i++;                
-        if (i < 100000) {       
-            loop();            
-        }                      
-    }, 500)
+if (typeof window.tweetCollectorDict === 'undefined') {
+    window.tweetCollectorDict = {};
 }
-loop();
+if (typeof window.tweetCollectorUrlSet === 'undefined') {
+    window.tweetCollectorUrlSet = new Set();
+}
+
+function collectTweetData() {
+    let tweetElements = document.querySelectorAll('[data-testid="tweet"]');
+    tweetElements.forEach(element => {
+        try {
+            let replyElement = element.querySelector('[data-testid="reply"]');
+            let retweetElement = element.querySelector('[data-testid="retweet"]');
+            let likeElement = element.querySelector('[data-testid="like"]');
+            let timeElement = element.querySelector('time');
+            let textElement = element.querySelector('[data-testid="tweetText"]');
+            let tweetLink = element.querySelector('a[href*="/status/"]');
+
+            let reply = replyElement ? replyElement.textContent.match(/(\d+)/) : null;
+            let retweet = retweetElement ? retweetElement.textContent.match(/(\d+)/) : null;
+            let like = likeElement ? likeElement.textContent.match(/(\d+)/) : null;
+            let time = timeElement ? timeElement.getAttribute('datetime') : null;
+            let status = tweetLink ? tweetLink.href.match(/\/status\/(\d+)/) : null;
+
+            if (textElement && time && tweetLink) {
+                let href = tweetLink.getAttribute('href');
+                let userId = element.querySelector('a[href^="/"][role="link"]').getAttribute('href').replace('/', '');
+                let fullUrl = `https://x.com${href.replace('/i/status/', `/${userId}/status/`)}`;
+                
+                if (!fullUrl.match(/\/(photo|video|media)\/\d+$/)) {
+                    window.tweetCollectorUrlSet.add(fullUrl);
+                    window.tweetCollectorDict[textElement.textContent.split("\n")[0] + "-" + time] = {
+                        "like": like ? like[1] : "0",
+                        "reply": reply ? reply[1] : "0",
+                        "retweet": retweet ? retweet[1] : "0",
+                        "time": time,
+                        "text": textElement.textContent,
+                        "status": status ? status[1] : "",
+                        "url": fullUrl
+                    };
+                }
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    });
+}
+
+function exit() {
+    window.collectedUrls = Array.from(window.tweetCollectorUrlSet);
+    clearTimeout(window.tweetCollectorLoopTimeout);
+}
+
+function loop() {
+    let i = 0;
+    function iterate() {
+        window.tweetCollectorLoopTimeout = setTimeout(function() {
+            window.scrollBy(0, 1000);
+            collectTweetData();
+            
+            i++;
+            if (i < 100000) {
+                iterate();
+            } else {
+                exit();
+            }
+        }, 500);
+    }
+    iterate();
+}
+
+if (typeof window.tweetCollectorLoopTimeout === 'undefined') {
+    loop();
+}
